3607eb2b9fbac168d8714292c3ec9ecc93911bfd
frank.cornelis <frank.cornelis@ae532bfc-2517-11de-9078-ef7f9e05793c>
2011-01-06 08:55:11 +0000

    preparing for next development cycle

--- eid-trust-service-tests/src/test/java/test/integ/be/fedict/trust/ReleaseTest.java
+++ eid-trust-service-tests/src/test/java/test/integ/be/fedict/trust/ReleaseTest.java
@@ -46,7 +46,6 @@ import org.apache.commons.logging.LogFactory;
 import org.junit.Test;
 import org.w3c.dom.Document;
 import org.w3c.dom.Node;
-import org.w3c.dom.NodeList;
 import org.xml.sax.SAXException;
 
 import com.sun.org.apache.xpath.internal.XPathAPI;
@@ -63,7 +62,7 @@ public class ReleaseTest {
 	private static final Log LOG = LogFactory.getLog(ReleaseTest.class);
 
 	// 1.0.0-SNAPSHOT
-	private static final String CURRENT_VERSION = "1.0.0.RC3";
+	private static final String CURRENT_VERSION = "1.0.0.RC4";
 
 	// 1.0.0.RC1
 	private static final String NEW_VERSION = "1.0.0-SNAPSHOT";
@@ -78,12 +77,10 @@ public class ReleaseTest {
 		URL classUrl = classLoader.getResource(classResourceName);
 		LOG.debug("class URL: " + classUrl);
 		URL baseUrl = new URL(
-				classUrl
-						.toString()
+				classUrl.toString()
 						.substring(
 								0,
-								classUrl
-										.toString()
+								classUrl.toString()
 										.indexOf(
 												"eid-trust-service-tests/target/test-classes/test/integ/be/fedict/trust/ReleaseTest.class")));
 		LOG.debug("base URL: " + baseUrl);
@@ -94,20 +91,6 @@ public class ReleaseTest {
 		for (File pomFile : pomFiles) {
 			LOG.debug("pom.xml: " + pomFile.getAbsolutePath());
 			Document pomDocument = loadDocument(pomFile);
-			NodeList dependencyVersionTextNodeList = XPathAPI
-					.selectNodeList(
-							pomDocument.getDocumentElement(),
-							"//:dependency[:groupId/text()='be.fedict.eid-trust-service']/:version/text()",
-							pomDocument.getDocumentElement());
-			LOG.debug("# dependency nodes: "
-					+ dependencyVersionTextNodeList.getLength());
-			for (int idx = 0; idx < dependencyVersionTextNodeList.getLength(); idx++) {
-				Node dependencyVersionTextNode = dependencyVersionTextNodeList
-						.item(idx);
-				assertEquals(CURRENT_VERSION, dependencyVersionTextNode
-						.getNodeValue());
-				dependencyVersionTextNode.setNodeValue(NEW_VERSION);
-			}
 
 			Node projectVersionTextNode = XPathAPI
 					.selectSingleNode(
@@ -115,8 +98,8 @@ public class ReleaseTest {
 							"/:project[:groupId[contains(text(), 'be.fedict.eid-trust-service')]]/:version/text()",
 							pomDocument.getDocumentElement());
 			if (null != projectVersionTextNode) {
-				assertEquals(CURRENT_VERSION, projectVersionTextNode
-						.getNodeValue());
+				assertEquals(CURRENT_VERSION,
+						projectVersionTextNode.getNodeValue());
 				projectVersionTextNode.setNodeValue(NEW_VERSION);
 			}
 
@@ -127,20 +110,9 @@ public class ReleaseTest {
 							"/:project[:groupId[text() = 'be.fedict'] and :artifactId[contains(text(), 'eid-trust-service')]]/:version/text()",
 							pomDocument.getDocumentElement());
 			if (null != projectVersionTextNode) {
-				assertEquals(CURRENT_VERSION, projectVersionTextNode
-						.getNodeValue());
+				assertEquals(CURRENT_VERSION,
+						projectVersionTextNode.getNodeValue());
 				projectVersionTextNode.setNodeValue(NEW_VERSION);
-			}
-
-			Node parentVersionTextNode = XPathAPI
-					.selectSingleNode(
-							pomDocument.getDocumentElement(),
-							"/:project/:parent[:groupId[contains(text(), 'be.fedict')] and :artifactId[contains(text(), 'eid-trust-service')]]/:version/text()",
-							pomDocument.getDocumentElement());
-			if (null != parentVersionTextNode) {
-				assertEquals(CURRENT_VERSION, parentVersionTextNode
-						.getNodeValue());
-				parentVersionTextNode.setNodeValue(NEW_VERSION);
 			}
 
 			storeDocument(pomDocument, pomFile);
@@ -167,7 +139,7 @@ public class ReleaseTest {
 		documentBuilderFactory.setNamespaceAware(true);
 		DocumentBuilder documentBuilder = documentBuilderFactory
 				.newDocumentBuilder();
-        return documentBuilder.parse(documentInputStream);
+		return documentBuilder.parse(documentInputStream);
 	}
 
 	private void storeDocument(Document document, File file)
