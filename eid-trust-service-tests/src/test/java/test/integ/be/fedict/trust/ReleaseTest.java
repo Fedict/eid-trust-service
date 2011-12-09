c52a267ec4421a5fe7ca9abba9998e12d9913322
frank.cornelis <frank.cornelis@ae532bfc-2517-11de-9078-ef7f9e05793c>
2011-12-09 11:01:26 +0000

    preparing for next development cycle

--- eid-trust-service-tests/src/test/java/test/integ/be/fedict/trust/ReleaseTest.java
+++ eid-trust-service-tests/src/test/java/test/integ/be/fedict/trust/ReleaseTest.java
@@ -60,10 +60,10 @@ public class ReleaseTest {
 	private static final Log LOG = LogFactory.getLog(ReleaseTest.class);
 
 	// 1.0.0-SNAPSHOT
-	private static final String CURRENT_VERSION = "1.0.0.RC10";
+	private static final String CURRENT_VERSION = "1.0.0.GA";
 
 	// 1.0.0.RC1
-	private static final String NEW_VERSION = "1.0.0-SNAPSHOT";
+	private static final String NEW_VERSION = "1.0.1-SNAPSHOT";
 
 	@Test
 	public void testVersioning() throws Exception {
