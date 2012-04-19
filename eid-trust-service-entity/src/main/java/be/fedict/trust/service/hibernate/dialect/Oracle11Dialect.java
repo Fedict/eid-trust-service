f07fb7511ec5a8fe5cd08dcfcec42365e5293c39
frank.cornelis <frank.cornelis@ae532bfc-2517-11de-9078-ef7f9e05793c>
2012-04-19 11:48:02 +0000

    fixed copyright notice

--- eid-trust-service-entity/src/main/java/be/fedict/trust/service/hibernate/dialect/Oracle11Dialect.java
+++ eid-trust-service-entity/src/main/java/be/fedict/trust/service/hibernate/dialect/Oracle11Dialect.java
@@ -1,8 +1,19 @@
 /*
- * SafeOnline project.
+ * eID Trust Service Project.
+ * Copyright (C) 2009-2012 FedICT.
  *
- * Copyright 2006 Lin.k N.V. All rights reserved.
- * Lin.k N.V. proprietary/confidential. Use is subject to license terms.
+ * This is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License version
+ * 3.0 as published by the Free Software Foundation.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this software; if not, see 
+ * http://www.gnu.org/licenses/.
  */
 
 package be.fedict.trust.service.hibernate.dialect;
